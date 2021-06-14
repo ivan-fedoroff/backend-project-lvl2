7fceabb061bb1352d785649bf071f8f4856e48a5
Fedorov Ivan <sirarthurjohnevans@live.com>
2021-06-14 16:37:07 +0300

    add format flag ('-f, --format [type]')

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,12 @@ const program = new Command();
 
 program
   .version('0.1.0', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('[type]');
 
 program.parse(process.argv);
 
+const options = program.opts();
+if (options.format) console.log(`- ${options.format}`);
 // export default program;
